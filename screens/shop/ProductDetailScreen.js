a538c7e486b640611ce1b7207aa1af14ffb67335
Tanner Schmutte <tannerschmutte@gmail.com>
2021-07-19 15:51:29 -0700

    display content on ProductDetailScreen

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -9,6 +9,8 @@ import {
 } from 'react-native';
 import { useSelector } from 'react-redux';
 
+import Colors from '../../constants/Colors';
+
 const ProductDetailScreen = (props) => {
     const productId = props.navigation.getParam('productId');
 
@@ -17,18 +19,53 @@ const ProductDetailScreen = (props) => {
     );
 
     return (
-        <View>
-            <Text>{selectedProduct.title}</Text>
-        </View>
+        <ScrollView>
+            <Image
+                style={styles.image}
+                source={{ uri: selectedProduct.imageUrl }}
+            />
+            <View style={styles.action}>
+                <Button
+                    color={Colors.primary}
+                    title="Add to Cart"
+                    onPress={() => {}}
+                />
+            </View>
+            <Text style={styles.price}>${selectedProduct.price}</Text>
+            <Text style={styles.description}>
+                {selectedProduct.description}
+            </Text>
+        </ScrollView>
     );
 };
 
-ProductDetailScreen.navigationOptions = navData => {
+ProductDetailScreen.navigationOptions = (navData) => {
     return {
-        headerTitle: navData.navigation.getParam('productTitle')
-    }
-}
+        headerTitle: navData.navigation.getParam('productTitle'),
+    };
+};
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 300,
+    },
+    price: {
+        fontSize: 20,
+        color: '#888',
+        textAlign: 'center',
+        marginVertical: 20,
+    },
+    description: {
+        fontSize: 14,
+        textAlign: 'center',
+        marginHorizontal: 20,
+    },
+    action: {
+        marginVertical: 10,
+        alignItems: 'center',
+
+    }
+});
 
 export default ProductDetailScreen;
