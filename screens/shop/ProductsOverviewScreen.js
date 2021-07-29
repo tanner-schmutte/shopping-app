df8420bd7594fc259575e561ee211bf2f3caa7f6
Tanner Schmutte <tannerschmutte@gmail.com>
2021-07-28 20:14:15 -0700

    display loading spinner and handle errors

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,5 +1,12 @@
-import React, { useEffect } from 'react';
-import { FlatList } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+    View,
+    Text,
+    FlatList,
+    AcitivityIndicator,
+    StyleSheet,
+    Button,
+} from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
@@ -7,14 +14,57 @@ import HeaderButton from '../../components/UI/HeaderButton';
 import ProductItem from '../../components/shop/ProductItem';
 import * as cartActions from '../../store/actions/cart';
 import * as productActions from '../../store/actions/products';
+import Colors from '../../constants/Colors';
 
 const ProductsOverviewScreen = (props) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
     const products = useSelector((state) => state.products.availableProducts);
     const dispatch = useDispatch();
 
+    const loadProducts = useCallBack(async () => {
+        setError(null);
+        setIsLoading(true);
+        try {
+            await dispatch(productActions.fetchProducts());
+        } catch (err) {
+            setError(err.message);
+        }
+        setIsLoading(false);
+    }, [dispatch, setIsLoading, setError]);
+
     useEffect(() => {
-        dispatch(productActions.fetchProducts());
-    }, [dispatch]);
+        loadProducts();
+    }, [dispatch, loadProducts]);
+
+    if (error) {
+        return (
+            <View style={styles.centered}>
+                <Text>An error occurred.</Text>
+                <Button
+                    title="Try Again"
+                    onPress={loadProducts}
+                    color={Colors.primary}
+                />
+            </View>
+        );
+    }
+
+    if (isLoading) {
+        return (
+            <View style={styles.centered}>
+                <AcitivityIndicator size="large" color={Colors.primary} />
+            </View>
+        );
+    }
+
+    if (!isLoading && products.length === 0) {
+        return (
+            <View style={styles.centered}>
+                <Text>No products found. Try adding some!</Text>
+            </View>
+        );
+    }
 
     return (
         <FlatList
@@ -66,5 +116,13 @@ ProductsOverviewScreen.navigationOptions = (navData) => {
         ),
     };
 };
+
+const styles = StyleSheet.create({
+    centered: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
 
 export default ProductsOverviewScreen;
--- store/actions/products.js
+++ store/actions/products.js
@@ -7,27 +7,36 @@ export const SET_PRODUCTS = 'SET_PRODUCTS';
 
 export const fetchProducts = () => {
     return async (dispatch) => {
-        const response = await fetch(
-            'https://shopping-app-b757b-default-rtdb.firebaseio.com/products.json'
-        );
-
-        const json = await response.json();
-        const loadedProducts = [];
-
-        for (const key in json) {
-            loadedProducts.push(
-                new Product(
-                    key,
-                    'u1',
-                    json[key].title,
-                    json[key].imageUrl,
-                    json[key].description,
-                    json[key].price
-                )
+        try {
+            const response = await fetch(
+                'https://shopping-app-b757b-default-rtdb.firebaseio.com/products.json'
             );
-        }
 
-        dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const json = await response.json();
+            const loadedProducts = [];
+
+            for (const key in json) {
+                loadedProducts.push(
+                    new Product(
+                        key,
+                        'u1',
+                        json[key].title,
+                        json[key].imageUrl,
+                        json[key].description,
+                        json[key].price
+                    )
+                );
+            }
+
+            dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+        } catch (err) {
+            // send to custom analytics server
+            throw err;
+        }
     };
 };
 
