738b8fc20ab547b755dfe824a0fe5639f1afed03
Tanner Schmutte <tannerschmutte@gmail.com>
2021-08-13 15:04:59 -0700

    implement 'Auto Login' with AsyncStorage

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,5 +1,14 @@
-export const SIGNUP = 'SIGNUP';
-export const LOGIN = 'LOGIN';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const AUTHENTICATE = 'AUTHENTICATE';
+
+export const authenticate = (userId, token) => {
+    return {
+        type: AUTHENTICATE,
+        userId: userId,
+        token: token,
+    };
+};
 
 export const signup = (email, password) => {
     return async (dispatch) => {
@@ -29,11 +38,13 @@ export const signup = (email, password) => {
             throw new Error(message);
         }
 
-        dispatch({
-            type: SIGNUP,
-            token: resData.idToken,
-            userId: resData.localId,
-        });
+        dispatch(authenticate(resData.localId, resData.idToken));
+
+        const expirationDate = new Date(
+            new Date().getTime() + parseInt(resData.expiresIn) * 1000
+        );
+
+        saveDataToStorage(resData.idToken, resData.localId, expirationDate);
     };
 };
 
@@ -67,10 +78,23 @@ export const login = (email, password) => {
             throw new Error(message);
         }
 
-        dispatch({
-            type: LOGIN,
-            token: resData.idToken,
-            userId: resData.localId,
-        });
+        dispatch(authenticate(resData.localId, resData.idToken));
+
+        const expirationDate = new Date(
+            new Date().getTime() + parseInt(resData.expiresIn) * 1000
+        );
+
+        saveDataToStorage(resData.idToken, resData.localId, expirationDate);
     };
+};
+
+const saveDataToStorage = (token, userId, expirationDate) => {
+    AsyncStorage.setItem(
+        'userData',
+        JSON.stringify({
+            token: token,
+            userId: userId,
+            expiryDate: expirationDate.toISOString(),
+        })
+    );
 };
